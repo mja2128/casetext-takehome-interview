c6c2fe3e69bed1406adb11dd3f8d86bd0d1bccf3
Mike Angerville <mja2128@gmail.com>
2023-09-27 18:59:46 -0400

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+
+const port = 5001;
+app.listen(port, () => {
+    /* eslint-disable no-console */
+    console.log(`Listening: http://localhost:${port}`);
+    /* eslint-enable no-console */
+});
